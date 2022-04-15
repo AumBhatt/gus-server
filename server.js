767228325e90d4e5532b3461586e443f2c1e3a88
AumBhatt <aum9698@outlook.com>
2022-04-15 22:59:40 +0400

    Made body-based POST requests.

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
-import 'dotenv/config'
+import bodyParser from 'body-parser'
+import 'dotenv/config';
 
 import {getGitHubData, clearRedisCache} from './redisClient.js';
 
@@ -11,31 +12,58 @@ const PORT = 3030;
 
 // Solving the CORS issue for local development.
 app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
+/*
+    My initial idea was to pass username to be searched as a parameter in 
+    url, then extract parameter from request url and pass it to the getGitHubData.
+*/
 // Accept a POST request from client with the '/api/search' endpoint.
-app.post('/api/search/:username', (request, response) => {
-    console.log(request.params);
+app.post('/api/search', (request, response) => {
 
-    getGitHubData(request.params.username)
-    .then((resData) => { 
-        console.log("resData:", resData)
+    const requestBody = request.body;
+    if(!requestBody) {
         response.set('Content-Type', 'application/json');
-        if(resData !== -1) {
-            response.send(resData);
-            response.end();
-        }
-        else {
-            response.send(
-                JSON.stringify(
-                    {
-                        errorCode: -1,
-                        error: "Could not retrieve data from GitHub."
-                    }
-                )
-            );
-            response.end();
-        }
-    });
+        response.send(
+            JSON.stringify({
+                error: -2,
+                error: "request body missing"
+            })
+        );
+        response.end();
+    }
+    else if(requestBody.searchType === 'users' && requestBody.searchText)
+        getGitHubData(requestBody.searchText)
+        .then((resData) => { 
+            // console.log("resData:", resData)
+            response.set('Content-Type', 'application/json');
+            if(resData !== -1) {
+                response.send(resData);
+                response.end();
+            }
+            else {
+                response.send(
+                    JSON.stringify(
+                        {
+                            errorCode: -1,
+                            error: "Could not retrieve data from GitHub."
+                        }
+                    )
+                );
+                response.end();
+            }
+        });
+    else {
+        response.set('Content-Type', 'application/json');
+        response.send(
+            JSON.stringify({
+                error: -3,
+                error: "searchType or searchText keys missing in body of request."
+            })
+        );
+        response.end();
+    }
 });
 
 const setRedisData = () => {
